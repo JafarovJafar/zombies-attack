569a478a9b9a494a8e95e7d761669ccae020508e
Jafarov Jafar <jafarovja@yandex.ru>
2021-09-20 16:14:20 +0700

    wip

--- Assets/Scripts/GameSceneEntryPoint.cs
+++ Assets/Scripts/GameSceneEntryPoint.cs
@@ -4,16 +4,12 @@ public class GameSceneEntryPoint : MonoBehaviour
 {
     private void Start()
     {
-        _playerTouchInput.Init(_player);
-
         _zombieSpawner.Enable();
 
         _eventsPool = new EventsPool();
         _eventsPool.Init();
     }
 
-    [SerializeField] PlayerTouchInput _playerTouchInput;
-    [SerializeField] PlayerController _player;
     [SerializeField] ZombieSpawner _zombieSpawner;
     [SerializeField] EventsPool _eventsPool;
     [SerializeField] GameSceneUI _gameSceneUI;
