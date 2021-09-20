2a0ba24ec46f0937cdbf12172120e6d8bc90ffa8
Jafarov Jafar <jafarovja@yandex.ru>
2021-09-20 22:42:15 +0700

    wip

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] Button _startGameButton;
+    [SerializeField] Text _scoreText;
+
+    public void Init()
+    {
+
+    }
+}
