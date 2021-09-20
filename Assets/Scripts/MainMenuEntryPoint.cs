b8469cd96287e5f85271f23a3b04a9b21241b601
Jafarov Jafar <jafarovja@yandex.ru>
2021-09-20 22:42:15 +0700

    wip

---
+++ Assets/Scripts/MainMenuEntryPoint.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuEntryPoint : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    [SerializeField] MainMenuUI _mainMenuUI;
+
+    private void LoadGameScene()
+    {
+
+    }
+}
